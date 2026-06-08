623b47fc925d03dda1aee05c7b1758fb2a6bae9c
Avalos <sergio-hdd@hotmail.com>
2026-06-07 21:37:42 -0300

    'npm install @fortawesome/fontawesome-free' - 'yarn add bootstrap' - 'yarn add @popperjs/core' - los cambios en assets/app.js - 'yarn encore dev'

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,10 @@
 import './bootstrap.js';
 
-import './sb-admin/css/styles.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import * as bootstrap from 'bootstrap';
 
+window.bootstrap = bootstrap;
+
+import './sb-admin/css/styles.css';
 import './sb-admin/js/scripts.js';
+import '@fortawesome/fontawesome-free/css/all.min.css';
